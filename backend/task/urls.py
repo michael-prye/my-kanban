e10b6d02da6d57f0fef81b80d5538e49d564ee9f
michael-prye <michael.prye@gmail.com>
2023-03-07 14:22:56 -0500

    Updated project Urls to include Task Urls & Added basic Get and Post Request in Task Views with Sucsefull postman test

--- backend/task/urls.py
+++ backend/task/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from task import views
 
 urlpatterns=[
-    path('', views)
+    path('', views.task_list)
 ]
--- backend/task/views.py
+++ backend/task/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from rest_framework.decorators import api_view
+from rest_framework import status
+from rest_framework.response import Response
 
-# Create your views here.
+from .models import Task
+from .serializers import TaskSerializer
+
+@api_view(['GET','POST'])
+def task_list(request):
+    if request.method == 'GET':
+        task = Task.objects.all()
+        serializer = TaskSerializer(task, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+    elif request.method == 'POST':
+        serializer = TaskSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
