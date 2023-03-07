ac18b4a4bb90cc70d2a70b1b567c617b266b6e43
michael-prye <michael.prye@gmail.com>
2023-03-07 13:54:22 -0500

    Modified and removed code for auth

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,15 +4,12 @@ import "./App.css";
 
 // Pages Imports
 import HomePage from "./pages/HomePage/HomePage";
-import LoginPage from "./pages/LoginPage/LoginPage";
-import RegisterPage from "./pages/RegisterPage/RegisterPage";
+
 
 // Component Imports
 import Navbar from "./components/NavBar/NavBar";
 import Footer from "./components/Footer/Footer";
 
-// Util Imports
-import PrivateRoute from "./utils/PrivateRoute";
 
 function App() {
   return (
--- frontend/src/components/NavBar/NavBar.jsx
+++ frontend/src/components/NavBar/NavBar.jsx
@@ -1,11 +1,9 @@
 import React from "react";
-import { useContext } from "react";
 import { useNavigate, Link } from "react-router-dom";
-import AuthContext from "../../context/AuthContext";
 import "./NavBar.css";
 
 const Navbar = () => {
-  const { logoutUser, user } = useContext(AuthContext);
+
   const navigate = useNavigate();
   return (
     <div className="navBar">
@@ -16,11 +14,7 @@ const Navbar = () => {
           </Link>
         </li>
         <li>
-          {user ? (
-            <button onClick={logoutUser}>Logout</button>
-          ) : (
-            <button onClick={() => navigate("/login")}>Login</button>
-          )}
+  
         </li>
       </ul>
     </div>
