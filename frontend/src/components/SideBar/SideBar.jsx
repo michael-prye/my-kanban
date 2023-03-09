477358bef6a5fc37682bcc6d53996202ba12a6b9
michael-prye <michael.prye@gmail.com>
2023-03-09 11:08:58 -0500

    Added fetch function to GET projects to display on Sidenav + Map function to loop through all projects to display project names for dropdown

--- frontend/src/components/SideBar/SideBar.jsx
+++ frontend/src/components/SideBar/SideBar.jsx
@@ -1,15 +1,39 @@
 import { useNavigate } from "react-router-dom";
 import "./SideBar.css"
-
-
+import { useEffect, useState } from "react";
+import useFetch from "../../hooks/useFetch";
+import { ChevronDown, ChevronUp } from "react-bootstrap-icons";
 const SideBar = () => {
 
 const navigate = useNavigate();
+const [projects, getProjects] = useFetch('http://127.0.0.1:8000/api/project/','GET', null)
+const [projectDropdown, setProjectDropdown] = useState(false)
+
+useEffect(()=>{
+    getProjects();
+},[])
+
 
     return ( 
-        <div class='sidenav'>
-            <h6 onClick={()=> navigate("/")}>Dash Board</h6>
-            <h6 onClick={()=> navigate("/project")}>Projects</h6>
+        <div className='sidenav'>
+            <ul>
+                <li><h5 onClick={()=> navigate("/")}>Dash Board</h5></li>
+                <li><h5 onClick={()=> navigate("/project")}>Projects</h5>
+                    {projectDropdown ? <ChevronUp onClick={()=>setProjectDropdown(false)}/> : <ChevronDown onClick={()=>{setProjectDropdown(true)}}/>}</li>
+                    {projectDropdown == true &&
+                        <ul>
+                            {projects.map((project)=>{
+                                return(
+                                    <li><h4>{project.name}</h4></li>
+                                )
+                            })}
+                        </ul>
+                
+                        
+                    
+                    }
+            </ul>
+
         </div>
      );
 }
--- frontend/src/hooks/useFetch.js
+++ frontend/src/hooks/useFetch.js
@@ -1,3 +1,4 @@
+import React from "react";
 import { useEffect, useState } from "react";
 
 
@@ -5,24 +6,30 @@ const useFetch = (url,method,formData) => {
     const [data, setData] = useState([])
 
 
-
-    const fetchRequest = async (id,image)=>{
+    const fetchRequest = async (id)=>{
+        let finalUrl = ""
+        if (id){
+            finalUrl = url.concat("?id=",id)
+        }else{
+            finalUrl = url
+        }
+        console.log(finalUrl)
         if(method=="DELETE"){ // if else statement to handle if request returns a body of json text
             const response = await fetch(finalUrl,{
                 method: method,
-                headers:{Authorization: 'Bearer ' + token,
-                'Content-Type': 'application/json'},
+                headers:{'Content-Type': 'application/json'},
                 body: formData? JSON.stringify(formData) :null
                 })
                 .then((response)=>  response.text())
                 .then((data)=>setData((data))
                  )
+       
         }else{
             const response = await fetch(finalUrl,{
                 method: method,
-                headers:{Authorization: 'Bearer ' + token,
-                'Content-Type': 'application/json'},
-                body: formData? JSON.stringify(formData) :null
+                headers:{'Content-Type': 'application/json'},
+                body: formData? JSON.stringify(formData) :null,
+            
                 })
                 .then((response)=>  response.json())
                 .then((data)=>setData((data))
