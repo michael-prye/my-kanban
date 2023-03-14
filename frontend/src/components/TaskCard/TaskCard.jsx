7f396ac329999a9b961c9118a90e3b9b8547563b
michael-prye <michael.prye@gmail.com>
2023-03-14 15:25:47 -0400

    Created Modal on TaskCard Component to display task detials

--- frontend/src/components/TaskCard/TaskCard.jsx
+++ frontend/src/components/TaskCard/TaskCard.jsx
@@ -11,7 +11,8 @@ import { useState } from "react";
 const TaskCard = (props) => {
 
     const [data, putTaskStatus] = useFetch('http://127.0.0.1:8000/api/task/', 'PUT', null)
-
+    const [taskModal, showTaskModal] = useState(false)
+    const [taskForm, setTaskForm] = useState(props.task)
 
     const updateStatus = async(status, id)=>{
         await putTaskStatus(id, status);
@@ -21,8 +22,9 @@ const TaskCard = (props) => {
 
 
 
-    return ( 
-        <Container className="task-card">
+    return (
+        <div>
+        <Container className="task-card" onClick={()=>showTaskModal(true)}>
             <Row> <h6>{props.task.name}</h6></Row>
             <Row>
                 <Col><p className="task-description">{props.task.description}</p></Col>
@@ -35,11 +37,30 @@ const TaskCard = (props) => {
                     }
                 </Col>
             </Row>
-           
-            
-            
-
         </Container>
+        <Modal show={taskModal} onHide={()=>showTaskModal(false)} centered={true}>
+            <Modal.Header closeButton>
+                <Modal.Title>Task</Modal.Title>
+            </Modal.Header>
+            <Modal.Body>
+                <form className="task-form">
+                    <input className="task-input"
+                    type='text'
+                    name='name'
+                    value={taskForm.name}/>
+                    <textarea className="task-input"
+                    name="description"
+                    value={taskForm.description}
+                    rows='4'
+                    cols='30'/>
+                </form>
+            </Modal.Body>
+            <Modal.Footer>
+                <button onClick={()=>showTaskModal(false)}>Exit</button>
+            </Modal.Footer>
+            
+        </Modal>
+        </div>
      );
 }
  
