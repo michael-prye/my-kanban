07a5f7a0f2705c5a2dc5c48c1adf9e7cd4c20519
michael-prye <michael.prye@gmail.com>
2023-03-22 10:27:29 -0400

    Added Delete Function to Task Views

--- backend/task/views.py
+++ backend/task/views.py
@@ -8,7 +8,7 @@ from django.shortcuts import get_object_or_404
 from .models import Task
 from .serializers import TaskSerializer
 
-@api_view(['GET','POST','PUT'])
+@api_view(['GET','POST','PUT','DELETE'])
 @permission_classes([AllowAny])
 def task_list(request):
     if request.method == 'GET':
@@ -34,6 +34,15 @@ def task_list(request):
             serializer.save()
             return Response(serializer.data, status=status.HTTP_200_OK)
         return Response(serializer.error_messages, status=status.HTTP_400_BAD_REQUEST)
+    elif request.method == "DELETE":
+        task_id = request.query_params.get('id')
+        task = Task.objects.filter(pk = task_id)
+        task.delete()
+        return Response(status=status.HTTP_202_ACCEPTED)
+
+
+
+
     
 @api_view(['GET'])
 @permission_classes([AllowAny])
