3788ff9d1e16f6404f9e2ffd18d86eb41cac39d1
michael-prye <michael.prye@gmail.com>
2023-03-27 14:09:47 -0400

    Removed Axios request to remove Project dropdown + Added value to select tag to ProjectPage

--- frontend/src/components/SideBar/SideBar.jsx
+++ frontend/src/components/SideBar/SideBar.jsx
@@ -1,40 +1,20 @@
 import { useNavigate } from "react-router-dom";
 import "./SideBar.css"
-import { useEffect, useState } from "react";
-import { ChevronDown, ChevronUp } from "react-bootstrap-icons";
-import useAxios from "../../hooks/useAxios";
 
 const SideBar = () => {
 
 const navigate = useNavigate();
-const [projects, getProjects] = useAxios('http://127.0.0.1:8000/api/project/','GET', null)
-const [projectDropdown, setProjectDropdown] = useState(false)
 
-useEffect(()=>{
-    getProjects();
-},[])
+
+
 
 
     return ( 
         <div className='sidenav'>
             <ul>
                 <li><h5 onClick={()=> navigate("/")}>Dash Board</h5></li>
-                <li><h5 onClick={()=> navigate("/project")}>Projects</h5>
-                    {projectDropdown ? <ChevronUp onClick={()=>setProjectDropdown(false)}/> : <ChevronDown onClick={()=>{setProjectDropdown(true)}}/>}</li>
-                    {projectDropdown == true &&
-                        <ul>
-                            {projects.map((project)=>{
-                                return(
-                                    <li><h4>{project.name}</h4></li>
-                                )
-                            })}
-                        </ul>
-                
-                        
-                    
-                    }
+                <li><h5 onClick={()=> navigate("/project")}>Projects</h5></li>
             </ul>
-
         </div>
      );
 }
--- frontend/src/pages/ProjectPage/ProjectPage.js
+++ frontend/src/pages/ProjectPage/ProjectPage.js
@@ -6,6 +6,7 @@ const ProjectPage = () => {
 
     const [projectList, getProjectsList] = useAxios('http://127.0.0.1:8000/api/project/','GET', null)
     const [project, getProjects] = useAxios('http://127.0.0.1:8000/api/project/','GET', null)
+    const [projectSelect, setProjectSelect] = useState('DEFAULT')
     
 
     useEffect(()=>{
@@ -14,7 +15,14 @@ const ProjectPage = () => {
 
     const handleProjectSelect = async(e)=>{
         e.persist();
-        await getProjects(e.target.value,null,null)
+        if(e.target.value === 'new'){
+            console.log('Create New Project')
+        }
+        else{
+            setProjectSelect(e.target.value)
+            await getProjects(e.target.value,null,null)
+        }
+        
 
     }
 
@@ -22,20 +30,19 @@ const ProjectPage = () => {
 
     return ( 
         <div class="project-container">
-            <h1>ProjectPage</h1>
-            <button onClick={()=>console.log(project)}>show project</button>
-            <select value={project} onChange={handleProjectSelect}>
-                <option value='' disabled selected hidden>Select Project</option>
+            <select  value={projectSelect} onChange={handleProjectSelect}>
+                <option value='DEFAULT' disabled hidden  >Select Project</option>
+                <option value='new'>Add new project</option>
                 {projectList.map((proj)=>(
                     <option value={proj.id}>{proj.name}</option>
                 ))}
 
             </select>
-            {project && 
+            {project[0] && 
                 <h2>{project[0].name}</h2>
             }
-            
         </div>
+        
      );
 }
  
